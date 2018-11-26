b2c33904be2f941e2794cf0010bb3c658e01e67b
MIYE <mahadevan.iyer@barco.com>
2018-11-26 03:26:01 -0800

    Added lambda and api role and policies

--- lambda/parking.js
+++ lambda/parking.js
@@ -10,20 +10,12 @@ exports.handler = (event, context, callback) => {
       return;
     }
 
-    // Because we're using a Cognito User Pools authorizer, all of the claims
-    // included in the authentication token are provided in the request context.
-    // This includes the username as well as other attributes.
     const username = event.requestContext.authorizer.claims['cognito:username'];
-
-    // The body field of the event in a proxy integration is a raw string.
-    // In order to extract meaningful values, we need to first parse this string
-    // into an object. A more robust implementation might inspect the Content-Type
-    // header first and use a different parsing strategy based on that value.
-    const requestBody = JSON.parse(event.body);
-
-    const pickupLocation = requestBody.PickupLocation;
-
-    const area = findArea(pickupLocation);
+    // const requestBody = JSON.parse(event.body);
+    const parkingLocation = {};
+    parkingLocation.Latitude = event["queryStringParameters"]['Latitude'];
+    parkingLocation.Longitude = event["queryStringParameters"]['Longitude'];
+    const area = findArea(parkingLocation);
 
     findParking(username, area).then(data => {
         var spots = [];
@@ -38,9 +30,6 @@ exports.handler = (event, context, callback) => {
             spots.push(item);
           }
         });
-        // You can use the callback function to provide a return value from your Node.js
-        // Lambda functions. The first parameter is used for failed invocations. The
-        // second parameter specifies the result data of the invocation.
 
         // Because this Lambda function is called by an API Gateway proxy integration
         // the result object must use the following structure.
@@ -53,26 +42,18 @@ exports.handler = (event, context, callback) => {
         });
     }).catch((err) => {
         console.error(err);
-
-        // If there is an error during processing, catch it and return
-        // from the Lambda function successfully. Specify a 500 HTTP status
-        // code and provide an error message in the body. This will provide a
-        // more meaningful error response to the end client.
         errorResponse(err.message, context.awsRequestId, callback)
     });
 };
 
-// This is where you would implement logic to find the optimal unicorn for
-// this ride (possibly invoking another Lambda function as a microservice.)
-// For simplicity, we'll just pick a unicorn at random.
-function findArea(pickupLocation) {
-    console.log('Finding area for ', pickupLocation.Latitude, ', ', pickupLocation.Longitude);
+function findArea(parkingLocation) {
+    console.log('Finding area for ', parkingLocation.Latitude, ', ', parkingLocation.Longitude);
     return 1;
 }
 
 function findParking(username, area) {
     var params = {
-      TableName: 'miye-table1'
+      TableName: 'piot-status-table'
     };
     return ddb.scan(params).promise();
 }
